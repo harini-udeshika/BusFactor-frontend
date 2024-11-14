d024598390d92661fbd4155447fdc870b70dffaa
harini-udeshika <84621255+harini-udeshika@users.noreply.github.com>
2024-11-14 13:09:49 +0530

    navigation added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import Home from './Views/Home';
+import Graphs from './Views/Graphs';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/graphs' element={<Graphs />} />
+        </Routes>
+      </BrowserRouter>
+
     </div>
-  );
+
+  )
 }
 
-export default App;
+export default App
