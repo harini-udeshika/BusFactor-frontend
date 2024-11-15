8ac7979ba607f192faddf089a3335e03ae94da16
harini-udeshika <84621255+harini-udeshika@users.noreply.github.com>
2024-11-15 23:54:31 +0530

    modified api call

--- src/Components/SearchResultsList/SearchResultsList.jsx
+++ src/Components/SearchResultsList/SearchResultsList.jsx
@@ -1,25 +1,41 @@
-import React from 'react'
+import React, { useState }from 'react'
 import './SearchResultsList.scss'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { FaRegStar } from 'react-icons/fa';
 import axios from 'axios';
+import  {Mosaic} from 'react-loading-indicators'
+import { useNavigate } from 'react-router-dom';
 const SearchResultsList = ({ results }) => {
 
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
   const handleClick = (repo_url) => {
+    setLoading(true);
     axios.post('http://localhost:5000/generate_graphs', {
         url: repo_url
       })
       .then((response) => {
-        console.log(response.data); // Handle the server response as needed
+
+        console.log(response.data);
+        navigate('/graphs', { state: { data: response.data } });
+
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
-      });
+      })
+      .finally(() => {
+        setLoading(false); // Set loading to false when the request finishes
+    });
   };
   
   return (
     <div className="results-list">
-      {
+       {loading && (
+           <div className="loading-indicator">
+               <Mosaic color="#bb9afd" size="medium" text="" textColor="" />
+           </div>
+            )}
+      {!loading &&
         results.map((result, id) => {
           let stars = result.stargazers_count;
           if (stars > 1000) {
