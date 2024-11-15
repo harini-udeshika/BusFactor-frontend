07849cceae7a484462b75a12fd4de45ed15c720d
harini-udeshika <84621255+harini-udeshika@users.noreply.github.com>
2024-11-15 23:56:15 +0530

    add graphs view

--- src/Views/Graphs.jsx
+++ src/Views/Graphs.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+import GraphsDisplay from '../Components/GraphsDisplay/GraphsDisplay';
+import Navbar from '../Components/Navbar/Navbar';
 
-const Graphs = () => {
-  return (
-    <div>Graphs</div>
-  )
+function GraphsPage() {
+  const location = useLocation();
+  const { data } = location.state; // Extract graph data passed via navigate
+  const { network_graph, key_collab } = data;
+
+  return <>
+    <Navbar></Navbar>
+    <GraphsDisplay fullNetworkData={network_graph} keyCollaboratorsData={key_collab} />
+  </>;
 }
 
-export default Graphs
+export default GraphsPage;
