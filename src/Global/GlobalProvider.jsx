1de928cb5289ce366bf3ec4d4c83868cb9d45336
harini-udeshika <84621255+harini-udeshika@users.noreply.github.com>
2024-11-18 22:24:51 +0530

    handling global elements

---
+++ src/Global/GlobalProvider.jsx
@@ -0,0 +1,24 @@
+import React, { createContext, useState, useEffect, useContext } from 'react';
+
+export const GlobalContext = createContext();
+
+export const GlobalProvider = ({ children }) => {
+  const [globalData, setGlobalData] = useState(() => {
+    // Retrieve data from sessionStorage on initial load
+    const storedData = sessionStorage.getItem('globalData');
+    return storedData ? JSON.parse(storedData) : {};
+  });
+
+  // Save globalData to sessionStorage whenever it changes
+  useEffect(() => {
+    sessionStorage.setItem('globalData', JSON.stringify(globalData));
+  }, [globalData]);
+
+  return (
+    <GlobalContext.Provider value={{ globalData, setGlobalData }}>
+      {children}
+    </GlobalContext.Provider>
+  );
+};
+
+export const useMyContext = () => useContext(GlobalContext);
