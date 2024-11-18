ca36dcf1306cba612fb3b574facc5fa9630ef1bf
harini-udeshika <84621255+harini-udeshika@users.noreply.github.com>
2024-11-18 22:25:31 +0530

    modified global styles

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import React from 'react'
+import React from 'react';
 import { Route, BrowserRouter, Routes } from 'react-router-dom';
 import Home from './Views/Home';
 import Graphs from './Views/Graphs';
+import CompletedTasks from './Views/CompletedTasks';
+import Navbar from './Components/Navbar/Navbar';
 
 const App = () => {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/graphs' element={<Graphs />} />
+          <Route path='/completed-tasks' element={<CompletedTasks />} />
+          <Route path='*' element={<h1>404 Not Found</h1>} /> {/* Fallback Route */}
         </Routes>
       </BrowserRouter>
-
     </div>
+  );
+};
 
-  )
-}
-
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { GlobalProvider } from './Global/GlobalProvider'; // Use GlobalProvider here
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <GlobalProvider> {/* Correctly wrap the App with GlobalProvider */}
+      <App />
+    </GlobalProvider>
   </React.StrictMode>
 );
 
