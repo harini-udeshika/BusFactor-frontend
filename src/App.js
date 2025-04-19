f0b316c13a0b2dd7cc768f13812ae195392e6d9a
harini-udeshika <84621255+harini-udeshika@users.noreply.github.com>
2025-04-19 20:51:15 +0530

    modified nav

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Graphs from './Views/Graphs';
 import CompletedTasks from './Views/CompletedTasks/CompletedTasks';
 import Navbar from './Components/Navbar/Navbar';
 import FileContribution from './Views/FileContributions/FileContribution';
+import MarkdownRenderer from './Views/Documentation/Documentation';
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
           <Route path='/graphs/:repo/*' element={<Graphs />} />
           <Route path='/completed-tasks' element={<CompletedTasks />} />
           <Route path='/file-contribution/:name' element={<FileContribution />} />
+          <Route path='/documentation' element={<MarkdownRenderer />} />
           <Route path='*' element={<h1>404 Not Found</h1>} /> {/* Fallback Route */}
         </Routes>
       </BrowserRouter>
